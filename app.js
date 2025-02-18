e047eb1db2d1dafaa4c31936300472a6f2953856
Piwexx <vigarioignacio@gmail.com>
2025-02-18 16:50:53 -0300

    update

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-
+const { version } = require("./package.json")
 // get the port from env variable
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+app.get('/health',(req,res) => {
+  res.send('ok')
+})
+app.get('/version',(req,res) => {
+  res.send(version)
+})
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
